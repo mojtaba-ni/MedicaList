e62c70aa1c0903c63b234d2a51b4c67c69387bba
mojtaba-ni <mojtabanik@yahoo.com>
2022-02-05 02:00:48 +0330

    define Starter section

--- src/sections/Starter.jsx
+++ src/sections/Starter.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import workImage from '../assets/work.svg'
+import {GoPlay} from 'react-icons/go'
+import {HiOutlineArrowNarrowRight} from 'react-icons/hi'
+import '../styles/sections/Starter.scss'
+import Button from '../components/Button';
 
 const Starter = () => {
   return (
-      <div>
-          
+      <div className='main-container'>
+            <div className="container">
+                <div className='content'>
+                    <h1>
+                        We Provide Sulutions to Help You to Build or Grow  Your Business !
+                    </h1>
+                    <p>
+                        A professional web and mobile app development agency  with over 100+ web and app develop.
+                        We provide a high-quality service in web and mobile app development  as well as in design.
+                    </p>
+                    <div className="botton-container">
+                        <Button content="Watch Video" icon={<GoPlay/>}/>
+                        <Button content="Request Quote" icon={<HiOutlineArrowNarrowRight/>} color='pink'/>
+                    </div>
+                </div>
+                <div className="image">
+                    <img src={workImage} alt="" />
+                </div>
+            </div>
       </div>
   )
 };
