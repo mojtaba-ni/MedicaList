95e7ca8646d9ebef5be5718216e396707bb87723
mojtaba-ni <mojtabanik@yahoo.com>
2022-03-20 16:52:17 +0330

    define Card component darktheme

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { CardAnimations } from '../Animations';
 import { motion } from 'framer-motion';
+import { useSelector} from 'react-redux'; 
 import '../styles/components/Card.scss'
 
 const Card = ({title,Logo,animateCustom}) => {
+  const theme = useSelector(state=>state.theme.darkTheme)
   return (
-    <motion.div className='card' animate={animateCustom} variants={CardAnimations}>
+    <motion.div className={`card ${theme===true ? "darktheme" : ""}`} animate={animateCustom} variants={CardAnimations}>
         <div className="logo">{Logo}</div>
-        <div className="card-title">
+        <div className={`card-title ${theme===true ? "darktheme" : ""}`}>
             <h4>{title}</h4>
         </div>
     </motion.div>
