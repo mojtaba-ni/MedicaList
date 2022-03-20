26539ee987c16667cffc6ebbbe8342a649d574b6
mojtaba-ni <mojtabanik@yahoo.com>
2022-03-20 17:11:08 +0330

    define Testimonials layout darktheme

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -4,17 +4,19 @@ import Title from '../components/Title';
 import {HiLightBulb} from 'react-icons/hi';
 import { motion } from 'framer-motion';
 import { useScroll } from '../components/useScroll';
+import { useSelector} from 'react-redux'; 
 import { TopAnimations } from '../Animations';
 import "../styles/sections/Testimonials.scss"
 
 const Testimonials = () => {
     const [element,controls] = useScroll()
+    const theme = useSelector(state=>state.theme.darkTheme)
   return (
-    <div className='testimonials-container' id='testimonial' ref={element}>
+    <div className={`testimonials-container ${theme===true ? 'darktheme' : ''}`} id='testimonial' ref={element}>
         <div className="container">
             <motion.div className="top" animate={controls} variants={TopAnimations} transition={{delay:0.4}}>
                 <Title title='Testimonials' color='blue' LineCenter={true} />
-                <div className="subTitle">
+                <div className={`subTitle ${theme===true ? 'darktheme' : ''}`}>
                     <p>
                         Always stay Updated with the technologies thus  we help our clients 
                         by giving the best solutions for their needs  
